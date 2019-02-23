ede04bcc3f2ee77cc4ed9f222bc06c1d52206055
Shawn Wilson <shawn.wilson137@gmail.com>
2019-02-23 06:35:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  [...kittens, name]
+ return [...kittens, name]
 }
 
 function prependKitten(name){
-  [name, ...kittens]
+ return [name, ...kittens]
 }
 
 function removeLastKitten(){
